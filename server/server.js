ff6520a87f2cc79ff513b5aa41724a721c752a29
Jodena <jodenatham@gmail.com>
2025-03-07 01:23:36 +0800

    testing out routes

--- server/server.js
+++ server/server.js
@@ -3,10 +3,12 @@ import express from 'express';
 import 'dotenv/config';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import users from './routes/users.js';
+import users from './routes/userRoutes.js';
+import cors from 'cors';
 const port = process.env.PORT || 4000;
 
 const app = express();
+app.use(cors());
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
